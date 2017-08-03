561600a76d21b4b59c6b8d8197e1cd627d38ede8
Adrian Schaedle <adrian@schaedle.me>
2017-08-03 14:25:16 -0400

    Initial commit

---
+++ lib/colorgrid.ex
@@ -0,0 +1,18 @@
+defmodule Colorgrid do
+  @moduledoc """
+  Documentation for Colorgrid.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Colorgrid.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/colorgrid_test.exs
@@ -0,0 +1,8 @@
+defmodule ColorgridTest do
+  use ExUnit.Case
+  doctest Colorgrid
+
+  test "greets the world" do
+    assert Colorgrid.hello() == :world
+  end
+end
