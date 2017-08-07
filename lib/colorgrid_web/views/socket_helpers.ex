29ea4ff3c803f134caea0b20dd9a2a1f9754ac49
Adrian Schaedle <adrian@schaedle.me>
2017-08-07 09:15:49 -0400

    Build socket endpoint from socket definition
    Our Endpoint struct holds the info around what sockets are mounted. This
    changes the helper to accept the socket we're interested in, and looks
    up where it's mounted in that struct.

--- lib/colorgrid_web/views/socket_helpers.ex
+++ lib/colorgrid_web/views/socket_helpers.ex
@@ -3,11 +3,22 @@ defmodule ColorgridWeb.SocketHelpers do
   view and URL helpers for socket endpoints
   """
 
-  def socket_url(_conn) do
+  def socket_url(socket) do
     ColorgridWeb.Endpoint.url
     |> URI.parse
-    |> Map.update!(:scheme, fn scheme -> if (scheme == "https"), do: "wss", else: "ws" end)
-    |> Map.put(:path, "/socket/websocket")
+    |> Map.update!(:scheme, fn scheme -> websocket_scheme(scheme) end)
+    |> Map.put(:path, endpoint_for(socket) <> "/websocket")
     |> URI.to_string
+  end
+
+  defp websocket_scheme(scheme) do
+    if (scheme == "https"), do: "wss", else: "ws"
+  end
+
+  defp endpoint_for(socket) do
+    ColorgridWeb.Endpoint.__sockets__
+    |> Enum.filter(fn {_, sock} -> sock == socket end)
+    |> Enum.at(0)
+    |> elem(0)
   end
 end
